237ad040332acba0f6e49171f88e97274a08210c
Matthew <mray@zoll.com>
2022-03-20 18:25:01 -0400

    feat: updates to people page, full page layout only

---
+++ src/components/FormFields.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import "../styles/Forms.scss";
+
+export const FormFieldText = ({ label, value }) => {
+  return (
+    <div className="form-field">
+      <label>{label}</label>
+      <input type="text" placeholder={label} defaultValue={value} />
+    </div>
+  );
+};
+
+export const FormFieldEmail = ({ label, value }) => {
+  return (
+    <div className="form-field">
+      <label>{label}</label>
+      <input type="email" placeholder={label} defaultValue={value} />
+    </div>
+  );
+};
+
+export const FormFieldPhone = ({ label, value }) => {
+  return (
+    <div className="form-field">
+      <label>{label}</label>
+      <input type="tel" placeholder={label} defaultValue={value} />
+    </div>
+  );
+};
+
+export const FormFieldDate = ({ label, value }) => {
+  return (
+    <div className="form-field">
+      <label>{label}</label>
+      <input type="date" placeholder={label} defaultValue={value} />
+    </div>
+  );
+};
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,9 +5,14 @@ import "../styles/Layout.scss";
 
 export const Layout = ({ children }) => {
   return (
-    <main className="layout__main">
-      <SideNav />
-      <section className="layout__content">{children}</section>
+    <main>
+      <header className="layout__header">
+        <h1 className="layout__header--title">Scheduler</h1>
+      </header>
+      <section className="layout__main">
+        <SideNav />
+        <section className="layout__content">{children}</section>
+      </section>
     </main>
   );
 };
