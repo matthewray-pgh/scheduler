63e2ac2e4a99f32b984804e16b4dfbf4414955a5
Matthew <mray@zoll.com>
2023-05-24 19:19:13 -0400

    feat: added settings page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-import Header from "../components/Header.js";
 import "../styles/Home.scss";
 
 export const Home = () => {
   return (
     <main>
-      <Header />
       <h1>Home</h1>
     </main>
   );
