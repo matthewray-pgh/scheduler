1e3df0fb541f4754b85eb761d3a3189c011cc3ca
Matthew <mray@zoll.com>
2023-06-01 22:30:24 -0400

    feature: add styling to login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,6 +8,13 @@ import "../styles/Login.scss";
 import { FormFieldText } from "../components/FormFields.js";
 import { FormFieldButton } from "../components/FormFieldButton.js";
 
+import {
+  faCalendarAlt,
+  faUserClock,
+  faBusinessTime
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export const Login = () => {
   const [loginData, setLoginData] = useState({ user: "", password: "" });
   const [authorized, setAuthorized] = useState(false);
@@ -23,8 +30,20 @@ export const Login = () => {
       {authorized ? <Navigate to="/dashboard" /> : null}
       <main className="login__page">
         <section className="login__banner">
+          <FontAwesomeIcon
+            icon={faUserClock}
+            className="floating-icon--user-clock"
+          />
+          <FontAwesomeIcon
+            icon={faCalendarAlt}
+            className="floating-icon--calendar"
+          />
+          <FontAwesomeIcon
+            icon={faBusinessTime}
+            className="floating-icon--list"
+          />
           <div className="login__banner--content">
-            <h1>Scheduler</h1>
+            <h1>myScheduler</h1>
           </div>
         </section>
         <section className="login__form">
@@ -41,7 +60,7 @@ export const Login = () => {
                 forgot password
               </Link>
             </div>
-            <div style={{display: 'grid'}}>
+            <div style={{ display: "grid" }}>
               <FormFieldButton label="login" onClickHandler={handleSubmit} />
             </div>
           </div>
